6a35729c29d8b1c71dbba772797694f5ded93e0b
daniel <danielxleszczynski@gmail.com>
2016-03-18 23:48:26 +0100

    login fb

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var app = angular.module("krzychapp", ['ngRoute', 'ngMaterial']);
+    var app = angular.module("krzychapp", ['ngRoute', 'ngMaterial', 'facebook']);
 
     app.config(function($routeProvider) {
         $routeProvider
@@ -17,6 +17,23 @@
                 templateUrl : 'src/auth/view/login.html',
                 controller  : 'AuthController'
             });
-    });
+    })
+
+    .config([
+            'FacebookProvider',
+            function(FacebookProvider) {
+                var myAppId = '604046539746695';
+
+                // You can set appId with setApp method
+                FacebookProvider.setAppId('myAppId');
+
+                /**
+                 * After setting appId you need to initialize the module.
+                 * You can pass the appId on the init method as a shortcut too.
+                 */
+                FacebookProvider.init(myAppId);
+
+            }
+        ]);
 
 }());
