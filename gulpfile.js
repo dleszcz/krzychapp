0d5e9105bec2d25db91ba45dddd13cd9a4a8f79f
daniel <danielxleszczynski@gmail.com>
2016-03-18 18:38:51 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-// define tasks here
-gulp.task('default', function() {
-    // run tasks here
-    // set up watch handlers here
+gulp.task('sass', function () {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
 
+gulp.task('sass:watch', function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
 });
